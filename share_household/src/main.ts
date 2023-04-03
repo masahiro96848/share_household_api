943470c61c62acf543e6ba1641fecc696dbe6731
masahiro96848 <feetwcpic9436@gmail.com>
2023-04-03 22:18:08 +0900

    csrfトークンの実装を削除。またcookieに関するライブラリを削除

--- share_household/src/main.ts
+++ share_household/src/main.ts
@@ -1,18 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { Request } from 'express';
-import * as cookieParser from 'cookie-parser';
-import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix('api');
+  // CORS対応
   app.enableCors({
-    credentials: true,
     origin: ['http://localhost:3000', 'http://localhost'],
   });
-  app.use(cookieParser());
+
   await app.listen(3000);
 }
 bootstrap();
